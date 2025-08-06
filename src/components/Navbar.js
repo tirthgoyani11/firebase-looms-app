c4a2f8c423b50432c4361bf2671d3c96ffcf30e8
Tirth Goyani <131787779+tirthgoyani11@users.noreply.github.com>
2025-08-06 22:54:58 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Notification({ message }) {
-  if (!message) return null;
+function Navbar() {
   return (
-    <div className="notification">{message}</div>
+    <nav className="navbar">
+      <Link to="/">Dashboard</Link>
+      <Link to="/reports">Reports</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </nav>
   );
 }
-export default Notification;
+
+export default Navbar;
