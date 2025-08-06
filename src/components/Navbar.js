99c43745a766eac442d1d35221a608e96e91acbf
Tirth Goyani <131787779+tirthgoyani11@users.noreply.github.com>
2025-08-06 22:54:08 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Notification({ message }) {
+  if (!message) return null;
+  return (
+    <div className="notification">{message}</div>
+  );
+}
+export default Notification;
