a5d434f1cbbfa602406d2841ca218a06b7f55e95
Tirth Goyani <131787779+tirthgoyani11@users.noreply.github.com>
2025-08-06 23:30:40 +0530

    Add files via upload

---
+++ pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AddMachineForm from "../components/AddMachineForm";
+import MachineList from "../components/MachineList";
+import AddWorkerForm from "../components/AddWorkerForm";
+import WorkerList from "../components/WorkerList";
+import AddTakaForm from "../components/AddTakaForm";
+import TakaList from "../components/TakaList";
+import AddQualityForm from "../components/AddQualityForm";
+import QualityList from "../components/QualityList";
+
+function Dashboard() {
+  return (
+    <div className="dashboard
